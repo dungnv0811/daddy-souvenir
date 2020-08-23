57e49a63c1e047282d50748d764dedd3557ddf03
Dung Van Nguyen <Dung.Van.Nguyen@mgm-tp.com>
2020-08-23 15:13:23 +0700

    init commit

---
+++ src/main/java/com/dvnguyen/daddysouvenir/DaddySouvenirApplication.java
@@ -0,0 +1,13 @@
+package com.dvnguyen.daddysouvenir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaddySouvenirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaddySouvenirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvnguyen/daddysouvenir/DaddySouvenirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvnguyen.daddysouvenir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaddySouvenirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
